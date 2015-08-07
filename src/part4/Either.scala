675b91e815a208a8683e7a37679df5872fe7c1ee
FengZhou <zhoufengloop@gmail.com>
2015-08-07 23:19:47 +0800

    Update Either.

--- src/part4/Either.scala
+++ src/part4/Either.scala
@@ -8,6 +8,19 @@ sealed trait Either[+E, +A] {
     case Left(value) => Left(value)
     case Right(value) => Right(f(value))
   }
+  
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(value) => Left(value)
+    case Right(value) => f(value)
+  }
+  
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(value) => b
+    case Right(value) => this
+  }
+  
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = 
+    this flatMap (a => (b map (f(a, _))))
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
