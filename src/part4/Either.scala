fd8a0c6229b35fa74d1b5f156dcd7bce7f9a210c
FengZhou <zhoufengloop@gmail.com>
2015-08-08 12:08:02 +0800

    Add traverse and sequence in Either.

--- src/part4/Either.scala
+++ src/part4/Either.scala
@@ -25,3 +25,20 @@ sealed trait Either[+E, +A] {
 
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+    case Nil => Right(Nil)
+    case x :: xs => x flatMap (xx => (sequence(xs) map (xx :: _)))
+  }
+  
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case Nil => Right(Nil)
+    case x :: xs => f(x) flatMap (xx => traverse(xs)(f) map (xx :: _))
+  }
+  
+  def traverse2[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+    as.foldRight[Either[E, List[B]]](Right(Nil))((x, z) => f(x) flatMap (xx => (z map (xx :: _))))
+    
+  def sequence2[E, A](es: List[Either[E, A]]): Either[E, List[A]] = traverse(es)(x => x)
+}
