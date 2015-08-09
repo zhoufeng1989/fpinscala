5cdbd32ad0cda9d77964c3bf768c34990fd41ae3
FengZhou <zhoufengloop@gmail.com>
2015-08-10 06:58:15 +0800

    Add Stream.

--- src/part5/Stream.scala
+++ src/part5/Stream.scala
@@ -43,6 +43,17 @@ sealed trait Stream[+A] {
     foldRight[Stream[A]](Stream.empty)((a, b) => (if(p(a)) Stream.cons(a, b) else Stream.empty))
     
   def headOption: Option[A] = foldRight[Option[A]](None)((a, b) => Some(a))
+  
+  def map[B](f: A => B): Stream[B] = foldRight(Stream.empty[B])((a, b) => Stream.cons(f(a), b))
+  
+  def filter(f: A => Boolean): Stream[A] = foldRight(Stream.empty[A])((a, b) => (if (f(a)) Stream.cons(a, b) else b))
+  
+  def append[B >: A](s: Stream[B]): Stream[B] = foldRight(s)(Stream.cons(_, _))
+  
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Stream.empty[B])((a, b) => f(a) append(b))
+  
+  def find(p: A => Boolean): Option[A] = this.filter(p).headOption
+  
 }
 
 case object Empty extends Stream[Nothing]
@@ -59,4 +70,25 @@ object Stream {
   
   def apply[A](as: A*): Stream[A] = 
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+  
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+  
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+ 
+  val fibs: Stream[Int] = {
+      def go(f0: Int, f1: Int): Stream[Int] = 
+        cons(f0, go(f1, f0 + f1))
+      go(0, 1)
+    }
+ 
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((h, s)) => cons(h, unfold(s)(f))
+    case None => empty
+  }
+
+  val fibs2 = unfold((0, 1)){case (f0, f1) => Some((f0, (f1, f0 + f1)))}
+  
+  val ones = unfold(1)(a => Some(a, a))
+  
+  def from2(n: Int) = unfold(n)(a => Some((a, a + 1)))
 }
