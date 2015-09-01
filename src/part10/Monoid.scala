088d57ba25f9422704bf33491b97120861f3b671
FengZhou <zhoufengloop@gmail.com>
2015-09-01 08:55:18 +0800

    Add wcMonoid.

--- src/part10/Monoid.scala
+++ src/part10/Monoid.scala
@@ -91,5 +91,33 @@ object Monoid{
       foldMap(l zip l.tail, booleanAnd)(a => a._1 < a._2)
     }
   }
-}
 
+  sealed trait WC
+
+  case class Stub(chars: String) extends WC
+
+  case class Part(lStub: String, words: Int, rStub: String) extends WC
+
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    def op(x: WC, y: WC) = (x, y) match {
+      case (Stub(a), Stub(b)) => Stub(a + b)
+      case (Stub(a), Part(l, w, r)) => Part(a + l, w, r)
+      case (Part(l, w, r), Stub(b)) => Part(l, w, r + b)
+      case (Part(l1, w1, r1), Part(l2, w2, r2)) => Part(l1, w1 + w2 + (if((r1 + l2).isEmpty) 0 else 1), r2)
+    }
+
+    val zero = Stub("")
+  }
+
+  def count(chars: String): Int = {
+    def wc(c: Char): WC = if (c.isWhitespace) Part("", 0, "") else Stub(c.toString)
+    def unstub(s: String) = s.length min 1
+
+    foldMapV(chars.toIndexedSeq, wcMonoid)(wc) match {
+      case Stub(s) => unstub(s)
+      case Part(l, w, r) => unstub(l) + w + unstub(r)
+    }
+  }
+
+
+}
