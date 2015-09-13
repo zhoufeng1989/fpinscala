ac61369fb4a47099e1d2793d06bb9e7cf24332c8
FengZhou <zhoufengloop@gmail.com>
2015-09-13 21:12:33 +0800

    Add more to Monad.

--- src/part11/Monad.scala
+++ src/part11/Monad.scala
@@ -20,6 +20,24 @@ trait Monad[F[_]] extends Functor[F] {
   def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
     la.foldRight(unit(List[B]()))((a, lmb) => map2(f(a), lmb)(_ :: _))
 
+  def replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    sequence(List.fill(n)(ma))
+
+  def product[A, B](ma: F[A], mb: F[B]): F[(A, B)] = map2(ma, mb)((_, _))
+
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => flatMap(f(a))(g)
+
+  def flatMapViaCompose[A, B](ma: F[A])(f: A => F[B]): F[B] = {
+    def g = (_: Unit) => ma
+    compose(g, f)(())
+  }
+
+  def join[A](mma: F[F[A]]): F[A] =
+    flatMap(mma)(ma => ma)
+
+  def flatMap2[A, B](ma: F[A])(f: A => F[B]): F[B] =
+    join(map(ma)(f))
 }
 
 object Monad {
