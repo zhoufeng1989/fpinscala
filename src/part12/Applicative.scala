38ff84844f1b3d6586c06776687e338e84c94c3e
FengZhou <zhoufengloop@gmail.com>
2015-09-16 08:27:19 +0800

    Implement map2 via apply.

--- src/part12/Applicative.scala
+++ src/part12/Applicative.scala
@@ -4,8 +4,10 @@ import part11.Functor
  * Created by zhoufeng on 15-9-15.
  */
 trait Applicative[F[_]] extends Functor[F] {
-  // TODO, Implement map2 with apply
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(apply(unit(f.curried))(fa))(fb)
+
   def unit[A](a: =>A): F[A]
 
   //Implement apply with map2
@@ -26,5 +28,8 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((_, _))
+
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
 
 }
